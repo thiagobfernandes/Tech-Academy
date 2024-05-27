ea88fb6245e8e59b5d1cd39080298df3e01351d1
thiagobfernandes <thiago5437br@gmail.com>
2024-05-27 19:39:09 -0300

    Site de Jogos

---
+++ Site De Jogos/script/index.js
@@ -0,0 +1,15 @@
+const card = document.querySelectorAll('.desaparecer')
+
+
+const myobserver = new IntersectionObserver (entries => {
+entries.forEach(entry => {
+    if(entry.isIntersecting) {
+        entry.target.classList.add('aparecer')
+    } else
+    entry.target.classList.remove('aparecer')
+})
+
+
+})
+
+card.forEach( Element => myobserver.observe(Element));
